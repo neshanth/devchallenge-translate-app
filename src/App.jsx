350164132a665239f9e468a0a038be4b7a00d825
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-20 19:30:45 +0530

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Logo from "./components/Logo/Logo";
+
 const App = () => {
   return (
     <>
+      <header className="header-logo">
+        <Logo />
+      </header>
+
       <main className="main"></main>
     </>
   );
