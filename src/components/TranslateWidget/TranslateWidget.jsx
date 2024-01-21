063e8d43b25fa9ae205a26de63529d455461c0a4
neshanth <59483685+neshanth@users.noreply.github.com>
2024-01-21 11:24:15 +0530

    added api call and states for translation

--- src/components/TranslateWidget/TranslateWidget.jsx
+++ src/components/TranslateWidget/TranslateWidget.jsx
@@ -1,4 +1,5 @@
 import "./translateWidget.scss";
+import { useState } from "react";
 const TranslateWidget = () => {
   const buttons = [
     {
@@ -10,18 +11,44 @@ const TranslateWidget = () => {
       name: "French",
     },
   ];
+  const [translate, setTranslate] = useState("English");
+  const [text, setText] = useState("Hello How are you?");
+
+  const handleApiCallForLanguageTranslation = () => {
+    fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${translate}|fr`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleTranslateLanguageChange = (e, language) => {
+    setTranslate(language);
+  };
   return (
     <div className="translated-widget">
       <div className="language-options">
         {buttons.map((button) => (
-          <button key={button.id}>{button.name}</button>
+          <button className={`btn ${translate === button.name ? "selected-language" : ""}`} onClick={(e) => handleTranslateLanguageChange(e, button.name)} key={button.id}>
+            {button.name}
+          </button>
         ))}
       </div>
       <div className="language-input">
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea value={text} name="" id="" cols="30" rows="10" onChange={(e) => setText(e.target.value)}></textarea>
       </div>
       <div className="language-footer">
-        <button>Translate</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            handleApiCallForLanguageTranslation();
+          }}
+        >
+          Translate
+        </button>
       </div>
     </div>
   );
